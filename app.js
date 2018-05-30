63e965622b4173b6d1ad4828ae6f6bdb8b7cc3f6
Gregory Geller <gregorygeller@users.noreply.github.com>
2018-05-30 15:54:38 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 const express = require('express'),
-		https = require('https'),
 		http = require('http'),
 		fs = require('fs'),
 		app = express();
-
-const express_force_ssl = require('express-force-ssl');
-
-var options = {
-	key: fs.readFileSync('/private/etc/ssl/cert.pem'),
-	cert: fs.readFileSync('/private/etc/ssl/cert.pem')
-};
-
-app.use(express_force_ssl);
-
-http.createServer(app).listen(3000);
-https.createServer(options, app).listen(3001);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
